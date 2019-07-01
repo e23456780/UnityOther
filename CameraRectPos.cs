7a7b2e744b9330a3a8e082f92812c7e30473e9cc
陳阿威 <47232506+e23456780@users.noreply.github.com>
2019-07-01 14:57:50 +0800

    Update CameraRectPos.cs

--- CameraRectPos.cs
+++ CameraRectPos.cs
@@ -3,11 +3,21 @@ using UnityEngine;
 public class CameraRectPos
 {
     static Camera cam;
-    static Transform tag;
-    public static Vector3[] GetCamerRect(Camera _cam, Transform _tag)
+    static Transform floor;
+
+    /// <summary>
+    /// 用來取得攝影機對於目標(通常是地板) 
+    /// Used to get the camera for the target (usually the floor)
+    /// 所包含的範圍(以y軸為例)
+    /// range  (take the y-axis as an example)
+    /// </summary>
+    /// <param name="_cam"></param>
+    /// <param name="_floor"></param>
+    /// <returns></returns>
+    public static Vector3[] GetCamerRect(Camera _cam, Transform _floor)
     {
         cam = _cam;
-        tag = _tag;
+        floor = _floor;
 
         Vector3 p1 = ScenePos(cam.pixelWidth - 1, cam.pixelHeight - 1);
         Vector3 p2 = ScenePos(0.0f, cam.pixelHeight - 1);
@@ -19,8 +29,9 @@ public class CameraRectPos
     static Vector3 ScenePos(float width, float height)
     {
         Ray ray = cam.ScreenPointToRay(new Vector3(width, height));
-        float _h = ray.origin.y - tag.transform.position.y;
+        float _h = ray.origin.y - floor.transform.position.y;
         _h = Mathf.Abs(_h / ray.direction.y);
         return ray.GetPoint(_h);
     }
 }
+
